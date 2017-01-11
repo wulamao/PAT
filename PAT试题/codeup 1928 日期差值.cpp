b51d82885094baf85b3c783561af1cdbe3c645fc
zewei Wu <zewei Wu>
2017-01-11 18:21:57 +0800

    AC 就是开心

--- PAT试题/codeup 1928 日期差值.cpp
+++ PAT试题/codeup 1928 日期差值.cpp
@@ -18,63 +18,79 @@ date *maxDate,*minDate;
 
 void dateup(date *datex)
 {
+	bool leap_year=0;
+	/*
+	判断闰年一般的规律为: 
+	四年一闰,百年不闰,四百年再闰.其简单计算方法:
+	1.能被4整除而不能被100整除.（如2004年就是闰年,1800年不是.）
+	2.能被400整除.（如2000年是闰年）
+	*/
+	if((datex->year%4==0 && datex->year%100!=0) || (datex->year%400==0))
+	{
+		leap_year = 1;
+	}
+	else 
+	{
+		leap_year = 0;
+	}
+
 	datex->day++;
-	
-	if(datex->year%4==0 && datex->month==2)
+	switch(datex->month)
 	{
-		if(datex->day==30)
+		case 1:case 3:case 5:
+		case 7:case 8:case 10:
+		case 12:
 		{
-			datex->month++;
-			datex->day=1;
-		}
-	}
-	else if(datex->year%4!=0 && datex->month==2)
-	{
-		if(datex->day==29)
-		{
-			datex->month++;
-			datex->day=1;
-		}
-	}
-	else
-	{
-		switch(datex->month)
-		{
-			case 1:case 3:case 5:
-			case 7:case 8:case 10:
-			case 12:
+			if(datex->day==32)
 			{
-				if(datex->day==32)
+				datex->month++;
+				datex->day=1;
+				if(datex->month==13)
+				{
+					datex->year++;
+					datex->month=1;
+					datex->day=1;
+				}
+			}
+		}break;	
+		case 4:case 6:
+		case 9:case 11:
+		{
+			if(datex->day==31)
+			{
+				datex->month++;
+				datex->day=1;
+				if(datex->month==13)
+				{
+					datex->year++;
+					datex->month=1;
+					datex->day=1;
+				}	
+			}
+		}break;	
+		case 2:
+		{
+			if(leap_year)
+			{
+				if(datex->day==30)
 				{
 					datex->month++;
 					datex->day=1;
-					if(datex->month==13)
-					{
-						datex->year++;
-						datex->month=1;
-						datex->day=1;
-					}
-				}
-			}break;	
-			case 4:case 6:
-			case 9:case 11:
+				}	
+			}
+			else
 			{
-				if(datex->day==31)
+				if(datex->day==29)
 				{
 					datex->month++;
 					datex->day=1;
-					if(datex->month==13)
-					{
-						datex->year++;
-						datex->month=1;
-						datex->day=1;
-					}	
-				}
-			}break;	
-		}	
-	}
-	//Sleep(500);
-	//printf("%d %d %d\n",datex->year,datex->month,datex->day);
+				}	
+			}
+		}break;
+	}	
+		
+//	Sleep(500);
+//	printf("%d %d %d\n",datex->year,datex->month,datex->day);
 }
 
 bool judge_date(date* date1,date* date2)
@@ -124,40 +140,42 @@ void codeup_1928()
 	int num=0;
 	/*notice:return value of scanf()*/
 	/*notice:gets() can read string with ' '*/
-	//while(scanf("%c",&s[i++])!=EOF){break;};
+	//while(scanf("%c",&s[i++])!=EOF){break;};//input more items data
 	//sscanf()
 	//--atoi()
 	//while(1);
 	
-	gets(s1);//s1 must be initailized
+//	gets(s1);//s1 must be initailized
+//	gets(s2);//s2 must be initailized
 	
-	sscanf(s1  ,"%4s",year); 
-	sscanf(s1+4,"%2s",month); 
-	sscanf(s1+6,"%2s",day); 
-	
-	//puts(year);while(1);
-		
-	date1.year  = atoi(year);
-	date1.month = atoi(month);
-	date1.day   = atoi(day);
-	
-	gets(s2);//s2 must be initailized
-	
-	sscanf(s2  ,"%4s",year); 
-	sscanf(s2+4,"%2s",month); 
-	sscanf(s2+6,"%2s",day); 
-		
-	date2.year  = atoi(year);
-	date2.month = atoi(month);
-	date2.day   = atoi(day);
-	
-	while(judge_date(&date1,&date2))
+	while(scanf("%s%s",s1,s2)!=EOF)
 	{
-		num++;
-		dateup(minDate);
-		//printf("%d\n",num);
+		sscanf(s1  ,"%4s",year); 
+		sscanf(s1+4,"%2s",month); 
+		sscanf(s1+6,"%2s",day); 
+
+		date1.year  = atoi(year);
+		date1.month = atoi(month);
+		date1.day   = atoi(day);
+		
+		sscanf(s2  ,"%4s",year); 
+		sscanf(s2+4,"%2s",month); 
+		sscanf(s2+6,"%2s",day); 
+			
+		date2.year  = atoi(year);
+		date2.month = atoi(month);
+		date2.day   = atoi(day);
+		
+		while(judge_date(&date1,&date2))
+		{
+			num++;
+			dateup(minDate);
+			//printf("%d\n",num);
+		}
+		printf("%d\n",num+1);
+		num = 0;
 	}
-	printf("%d\n",num+1);
+	
 //	printf("%d %d %d\n",date1.year,date1.month,date1.day);
 //	printf("%d %d %d\n",date2.year,date2.month,date2.day);
 	
